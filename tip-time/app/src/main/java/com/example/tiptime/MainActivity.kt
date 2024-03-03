59717b43532268e79e2be864866496b0d09b10cf
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-03 18:36:21 +0300

    Tip round row added

--- tip-time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ tip-time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -158,3 +158,26 @@ fun EditNumberField(
         keyboardOptions = keyboardOptions
     )
 }
+
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+        Switch(
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+        )
+    }
+}
