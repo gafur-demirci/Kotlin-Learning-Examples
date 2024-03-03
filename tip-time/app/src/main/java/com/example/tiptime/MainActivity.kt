0c47531007b27c6d1a570ca0a829249833e1e8d3
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-03 18:35:17 +0300

    Start app fun changed

--- tip-time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ tip-time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TipTimeLayout()
                 }
             }
         }
