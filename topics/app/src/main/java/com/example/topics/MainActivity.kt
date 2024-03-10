fbc42885a2a56379903be96bbddc142510534997
gafur-demirci <gafur.gs.68@gmail.com>
2024-03-10 19:11:49 +0300

    Topics App configuration added

--- topics/app/src/main/java/com/example/topics/MainActivity.kt
+++ topics/app/src/main/java/com/example/topics/MainActivity.kt
@@ -3,13 +3,33 @@ package com.example.topics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.topics.data.DataSource.topics
+import com.example.topics.model.Topic
 import com.example.topics.ui.theme.TopicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TopicApp()
                 }
             }
         }
@@ -30,17 +50,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopicApp() {
+    TopicList()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TopicsTheme {
-        Greeting("Android")
+fun TopicList() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(start = 8.dp, top = 8.dp, end = 8.dp)
+    ) {
+        items(topics) { topic ->
+            TopicCard(topic)
+        }
     }
+}
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card {
+        Row {
+            Image(
+                painter = painterResource(topic.imageResourceId),
+                contentDescription = stringResource(topic.stringResourceId),
+                modifier = modifier
+                    .height(68.dp)
+                    .width(68.dp)
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop
+
+            )
+            Column(modifier = modifier.padding(top = 16.dp, end = 16.dp)) {
+                Row(modifier = modifier.padding(start = 16.dp)) {
+                    Text(
+                        text = LocalContext.current.getString(topic.stringResourceId),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                Row(
+                    modifier = modifier.padding(start = 16.dp, top = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = modifier.padding(end = 8.dp)
+
+                    )
+                    Text(
+                        text = topic.topicCount.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun TopicPreview() {
+    TopicCard(Topic(R.string.architecture, 58, R.drawable.architecture))
 }
