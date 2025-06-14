035c81409b642319dda88db1c2c71e21279b5311
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-06-14 17:17:50 +0300

    ICryptoAPI service added for send get request to get crypto currencies

---
+++ CryptoCurrencies/app/src/main/java/com/abdulgafur/demirci/cryptocurrencies/service/ICryptoAPI.kt
@@ -0,0 +1,12 @@
+package com.abdulgafur.demirci.cryptocurrencies.service
+
+import com.abdulgafur.demirci.cryptocurrencies.model.Currency
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ICryptoAPI {
+
+    @GET(value = "atilsamancioglu/K21-JSONDataSet/master/crypto.json")
+    fun getData(): Call<List<Currency>>
+
+}
