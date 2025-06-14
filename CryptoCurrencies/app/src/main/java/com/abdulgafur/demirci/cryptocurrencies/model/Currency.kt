9cf288e32f703f5cc3eb9f95512c5df923d9bd5a
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-06-14 17:25:15 +0300

    Remove unused import

--- CryptoCurrencies/app/src/main/java/com/abdulgafur/demirci/cryptocurrencies/model/Currency.kt
+++ CryptoCurrencies/app/src/main/java/com/abdulgafur/demirci/cryptocurrencies/model/Currency.kt
@@ -1,7 +1,5 @@
 package com.abdulgafur.demirci.cryptocurrencies.model
 
-import com.google.gson.annotations.SerializedName
-
 data class Currency(
 //    @SerializedName(value = "currency")
     val currency: String,
