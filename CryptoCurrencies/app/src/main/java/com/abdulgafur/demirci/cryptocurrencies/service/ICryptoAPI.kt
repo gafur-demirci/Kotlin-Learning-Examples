994f17a438edac48a43341f6449c073c9ea7f2e2
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-06-14 19:56:35 +0300

    Update ICryptoAPI to use Observable for reactive data handling

--- CryptoCurrencies/app/src/main/java/com/abdulgafur/demirci/cryptocurrencies/service/ICryptoAPI.kt
+++ CryptoCurrencies/app/src/main/java/com/abdulgafur/demirci/cryptocurrencies/service/ICryptoAPI.kt
@@ -1,12 +1,16 @@
 package com.abdulgafur.demirci.cryptocurrencies.service
 
 import com.abdulgafur.demirci.cryptocurrencies.model.Currency
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ICryptoAPI {
 
     @GET(value = "atilsamancioglu/K21-JSONDataSet/master/crypto.json")
-    fun getData(): Call<List<Currency>>
+    fun getData(): Observable<List<Currency>>
+
+
+//    fun getData(): Call<List<Currency>>
 
 }
