f95716226af4637972f521df8303eb525c06fbd7
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-06-14 21:31:20 +0300

    Define ICryptoAPI interface for fetching cryptocurrency data using Retrofit. Adds a suspend function getData() to retrieve a list of Currency objects from a remote JSON endpoint. This interface enables clean separation of network logic and supports coroutine-based asynchronous calls.

--- CryptoCurrencies/app/src/main/java/com/abdulgafur/demirci/cryptocurrencies/service/ICryptoAPI.kt
+++ CryptoCurrencies/app/src/main/java/com/abdulgafur/demirci/cryptocurrencies/service/ICryptoAPI.kt
@@ -1,16 +1,12 @@
 package com.abdulgafur.demirci.cryptocurrencies.service
 
 import com.abdulgafur.demirci.cryptocurrencies.model.Currency
-import io.reactivex.Observable
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface ICryptoAPI {
 
     @GET(value = "atilsamancioglu/K21-JSONDataSet/master/crypto.json")
-    fun getData(): Observable<List<Currency>>
-
-
-//    fun getData(): Call<List<Currency>>
+    suspend fun getData(): Response<List<Currency>>
 
 }
